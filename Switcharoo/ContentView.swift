dbb601ce1f2ea91981c6d9eb891ed864090b4464
dhruvshah8 <dhruvshah5784@gmail.com>
2020-06-23 13:53:27 -0400

    Initial Commit

---
+++ Switcharoo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Switcharoo
+//
+//  Created by Dhruv Shah on 2020-06-23.
+//  Copyright © 2020 Dhruv Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
